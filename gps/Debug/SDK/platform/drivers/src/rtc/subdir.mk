55f96bdcf9581a1b51f56d243ef1b7bce8157d96
juanpedro22 <juanpedro.bc22@gmail.com>
2025-09-18 11:34:11 -0300

    Initial commit: FRDM-KL25Z GPS sim (LAT/LON/ALT) via UART0 + OSA/PRINTF

---
+++ gps/Debug/SDK/platform/drivers/src/rtc/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../SDK/platform/drivers/src/rtc/fsl_rtc_common.c 
+
+OBJS += \
+./SDK/platform/drivers/src/rtc/fsl_rtc_common.o 
+
+C_DEPS += \
+./SDK/platform/drivers/src/rtc/fsl_rtc_common.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+SDK/platform/drivers/src/rtc/%.o: ../SDK/platform/drivers/src/rtc/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -D"FSL_OSA_BM_TIMER_CONFIG=2" -D"CPU_MKL25Z128VLK4" -D"PRINTF_ADVANCED_ENABLE=1" -D"PRINTF_FLOAT_ENABLE=1" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/hal/inc" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/hal/src/sim/MKL25Z4" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/system/src/clock/MKL25Z4" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/system/inc" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/osa/inc" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/CMSIS/Include" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/devices" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/devices/MKL25Z4/include" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/devices/MKL25Z4/startup" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/Generated_Code/SDK/platform/devices/MKL25Z4/startup" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/Sources" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/Generated_Code" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/utilities/src" -I"C:/Users/juan_/OneDrive/Documentos/Mestrado/Software Real Time/test2/gps/SDK/platform/utilities/inc" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ gps/Debug/SDK/platform/hal/src/sim/MKL25Z4/fsl_sim_hal_MKL25Z4.d
@@ -0,0 +1,38 @@
+SDK/platform/hal/src/sim/MKL25Z4/fsl_sim_hal_MKL25Z4.o: \
+ ../SDK/platform/hal/src/sim/MKL25Z4/fsl_sim_hal_MKL25Z4.c \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/fsl_device_registers.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/CMSIS/Include/core_cm0plus.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/CMSIS/Include/core_cmInstr.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/CMSIS/Include/core_cmFunc.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/Generated_Code/SDK/platform/devices/MKL25Z4/startup/system_MKL25Z4.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4_extension.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/fsl_bitaccess.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4_features.h \
+ C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/hal/inc/fsl_sim_hal.h \
+ c:\users\juan_\onedrive\documentos\mestrado\software\ real\ time\test2\gps\sdk\platform\hal\src\sim\mkl25z4\fsl_sim_hal_mkl25z4.h
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/fsl_device_registers.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/CMSIS/Include/core_cm0plus.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/Generated_Code/SDK/platform/devices/MKL25Z4/startup/system_MKL25Z4.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4_extension.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/fsl_bitaccess.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/devices/MKL25Z4/include/MKL25Z4_features.h:
+
+C:/Users/juan_/OneDrive/Documentos/Mestrado/Software\ Real\ Time/test2/gps/SDK/platform/hal/inc/fsl_sim_hal.h:
+
+c:\users\juan_\onedrive\documentos\mestrado\software\ real\ time\test2\gps\sdk\platform\hal\src\sim\mkl25z4\fsl_sim_hal_mkl25z4.h:
